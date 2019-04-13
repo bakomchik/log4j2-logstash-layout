f933ba02841cd323375cbd176b26bfdc271117c5
la_bakomchev <dmitriy.bakomchev@lamoda.ru>
2019-04-13 17:15:39 +0300

    added test to demonstrate stack overflow "bomb" in logstash layout

---
+++ layout/src/test/java/com/vlkan/log4j2/logstash/layout/LogStashLayoutGeneratorStateTest.java
@@ -0,0 +1,49 @@
+package com.vlkan.log4j2.logstash.layout;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.nio.BufferOverflowException;
+
+public class LogStashLayoutGeneratorStateTest {
+    private static final Configuration CONFIGURATION = new DefaultConfiguration();
+
+    @Test
+    public void testGeneratorStateCorruptedAfterUnhandledException() {
+        LogstashLayout layout = LogstashLayout
+                .newBuilder()
+                .setConfiguration(CONFIGURATION)
+                .setEventTemplateUri("classpath:LogstashTestLayout.json")
+                .setStackTraceEnabled(true)
+                .setLocationInfoEnabled(true)
+                .setEmptyPropertyExclusionEnabled(true)
+                .build();
+        LogEvent hugeMessageLogEvent = LogEventFixture.createHugeMeessagLogEvent(512 * 1024);
+        LogEvent fullEvent = LogEventFixture.createFullLogEvents(1).get(0);
+        long counter = 0;
+        encodeSafe(layout, fullEvent);
+        Assertions.assertThatThrownBy(()->layout.toByteArray(hugeMessageLogEvent)).hasCauseInstanceOf(BufferOverflowException.class);
+        long startTime = System.currentTimeMillis();
+        while(true){
+            encodeSafe(layout, fullEvent);
+            long l = ++counter;
+            if(System.currentTimeMillis()-startTime>=1000){
+                System.out.println(l);
+                startTime=System.currentTimeMillis();
+            }
+        }
+
+    }
+
+    private void encodeSafe(LogstashLayout layout, LogEvent fullEvent) {
+        try {
+            layout.toByteArray(fullEvent);
+        }
+        catch (Exception e){
+            //do nothing
+        }
+    }
+}
